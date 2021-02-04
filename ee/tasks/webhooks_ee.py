820d2efc724f55f6673a193cd8c12624c269bac2
Michael Matloka <dev@twixes.com>
2021-02-04 13:17:48 +0100

    Use transaction

--- ee/tasks/webhooks_ee.py
+++ ee/tasks/webhooks_ee.py
@@ -3,6 +3,7 @@ from typing import Any, Dict, Sequence, cast
 import requests
 from celery import Task
 from django.conf import settings
+from django.db import transaction
 from sentry_sdk import capture_exception
 
 from posthog.celery import app
@@ -14,7 +15,6 @@ from posthog.tasks.webhooks import determine_webhook_type, get_formatted_message
 def post_event_to_webhook_ee(self: Task, event: Dict[str, Any], team_id: int, site_url: str) -> None:
     if not site_url:
         site_url = settings.SITE_URL
-    postgres_event = None
     try:
         team = Team.objects.select_related("organization").get(pk=team_id)
         is_zapier_available = team.organization.is_feature_available("zapier")
@@ -27,40 +27,38 @@ def post_event_to_webhook_ee(self: Task, event: Dict[str, Any], team_id: int, si
                 actionFilters["post_to_slack"] = True  # We only need to fire for actions that are posted to webhook URL
         actions = cast(Sequence[Action], Action.objects.filter(**actionFilters).all())
 
-        postgres_event = Event.objects.create(
-            event=event["event"],
-            distinct_id=event["distinct_id"],
-            properties=event["properties"],
-            team=team,
-            site_url=site_url,
-            **({"timestamp": event["timestamp"]} if event["timestamp"] else {}),
-            **({"elements": event["elements_list"]} if event["elements_list"] else {})
-        )
+        with transaction.atomic():
+            transaction.set_rollback(True)  # Roll back Event creation on exit from the atomic block
+            ephemeral_postgres_event = Event.objects.create(
+                event=event["event"],
+                distinct_id=event["distinct_id"],
+                properties=event["properties"],
+                team=team,
+                site_url=site_url,
+                **({"timestamp": event["timestamp"]} if event["timestamp"] else {}),
+                **({"elements": event["elements_list"]} if event["elements_list"] else {})
+            )
 
-        for action in actions:
-            qs = Event.objects.filter(pk=postgres_event.pk).query_db_by_action(action)
-            if not qs:
-                continue
-            # REST hooks
-            if is_zapier_available:
-                action.on_perform(postgres_event)
-            # webhooks
-            if team.slack_incoming_webhook:
-                message_text, message_markdown = get_formatted_message(action, postgres_event, site_url,)
-                if determine_webhook_type(team) == "slack":
-                    message = {
-                        "text": message_text,
-                        "blocks": [{"type": "section", "text": {"type": "mrkdwn", "text": message_markdown},},],
-                    }
-                else:
-                    message = {
-                        "text": message_markdown,
-                    }
-                requests.post(team.slack_incoming_webhook, verify=False, json=message)
+            for action in actions:
+                qs = Event.objects.filter(pk=ephemeral_postgres_event.pk).query_db_by_action(action)
+                if not qs:
+                    continue
+                # REST hooks
+                if is_zapier_available:
+                    action.on_perform(ephemeral_postgres_event)
+                # webhooks
+                if team.slack_incoming_webhook:
+                    message_text, message_markdown = get_formatted_message(action, ephemeral_postgres_event, site_url,)
+                    if determine_webhook_type(team) == "slack":
+                        message = {
+                            "text": message_text,
+                            "blocks": [{"type": "section", "text": {"type": "mrkdwn", "text": message_markdown},},],
+                        }
+                    else:
+                        message = {
+                            "text": message_markdown,
+                        }
+                    requests.post(team.slack_incoming_webhook, verify=False, json=message)
     except:
         capture_exception()
         self.retry(countdown=(2 ** self.request.retries) / 2)
-    finally:
-        # Ensure that the temporary Postgres event is deleted
-        if postgres_event is not None:
-            postgres_event.delete()
